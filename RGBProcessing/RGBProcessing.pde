52a94ce5eaa1bdcd526aefaacdaa93b0bd7e9f4e
Ana2020143716 <anacsrodrigues.46@gmail.com>
2024-05-22 10:05:05 +0100

    cor

---
+++ RGBProcessing/RGBProcessing.pde
@@ -0,0 +1,70 @@
+
+
+import processing.serial.*;
+
+Serial port; 
+int canvasWidth = 400;
+int canvasHeight = 400;
+boolean dataReceived = false;
+int r = 0;
+int g = 0;
+int b = 0;
+
+void setup() {
+    
+    size(500,500);
+
+   
+    String[] portList = Serial.list();
+
+    
+    if (portList.length > 0) {
+        
+        String portName = "COM8";
+        
+        
+        port = new Serial(this, portName, 9600);
+    } else {
+        println("Nenhuma porta serial disponível");
+    }
+}
+
+void draw() {
+    
+    if (dataReceived) {
+      println("Cor recebida: R=" + r + ", G=" + g + ", B=" + b);
+      
+      background(r,g,b);
+      
+      if (r <=15 && g <=15 && b <=15){
+        background(255);
+      } else if (r<b && r<=g && r<23){
+        background(255,0,0);
+      } else if (b<g && b<r && b<20){
+        background(0,255,0);
+      } else if (g<r && g-b<= 8) {
+        background(0,0,255);
+      }
+              dataReceived = false;  
+    }
+}
+
+void serialEvent(Serial p) {
+
+    String data = p.readStringUntil('\n');
+    
+    if (data != null) {
+      
+        data = data.trim();
+        String[] rgb = data.split(",");
+
+        if (rgb.length == 3) {
+          
+            r = int(rgb[0]);
+            g = int(rgb[1]);
+            b = int(rgb[2]);
+
+            dataReceived = true;
+        }
+    }
+}
---
+++ RGBW/RGBW.ino
@@ -0,0 +1,71 @@
+
+#define s0 8  
+#define s1 9
+#define s2 10
+#define s3 11
+#define out 12
+
+int  Red=0, Blue=0, Green=0;  
+
+void setup() 
+{
+   pinMode(s0,OUTPUT);    
+   pinMode(s1,OUTPUT);
+   pinMode(s2,OUTPUT);
+   pinMode(s3,OUTPUT);
+   pinMode(out,INPUT);
+
+   Serial.begin(9600);  
+   
+   digitalWrite(s0,HIGH); 
+   digitalWrite(s1,HIGH); 
+   
+}
+
+void loop(){
+  
+  GetColors();                                     
+                                                   
+
+  if (Red <=15 && Green <=15 && Blue <=15) 
+      Serial.println("White");                    
+      
+  else if (Red<Blue && Red<=Green && Red<23)    
+      Serial.println("Red");
+
+  else if (Blue<Green && Blue<Red && Blue<20)  
+      Serial.println("Blue");
+
+  else if (Green<Red && Green-Blue<= 8)          
+      Serial.println("Green");                  
+
+  else
+     Serial.println("Unknown");                
+
+
+  delay(2000);                                  
+  
+  Serial.print(Red);
+    Serial.print(",");
+    Serial.print(Green);
+    Serial.print(",");
+    Serial.println(Blue);
+  
+}
+
+
+void GetColors()  
+{    
+  digitalWrite(s2,  LOW);                                           
+  digitalWrite(s3, LOW);                                           
+  Red = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);       
+  delay(20);  
+  digitalWrite(s3, HIGH);                                        
+  Blue = pulseIn(out, digitalRead(out) == HIGH ? LOW  : HIGH);
+  delay(20);  
+  digitalWrite(s2, HIGH);  
+  Green = pulseIn(out,  digitalRead(out) == HIGH ? LOW : HIGH);
+  delay(20);  
+}
+
+
