fbe0bb0e2c98bd4e5fb5dd505b5f680365f3e568
Ana2020143716 <anacsrodrigues.46@gmail.com>
2024-06-17 20:07:19 +0100

    +

--- RGBProcessing/RGBProcessing.pde
+++ RGBProcessing/RGBProcessing.pde
@@ -1,42 +1,36 @@
 import processing.serial.*;
 
-Serial port;
+Serial myPort;
 int redValue = 0;
 int greenValue = 0;
 int blueValue = 0;
 
-boolean dataReceived = false;
-
 void setup() {
   size(400, 400);
-
-  String[] portList = Serial.list();
-
-  if (portList.length > 0) {
-
-    String portName = "COM8";
-
-    port = new Serial(this, portName, 9600);
-  } else {
-    println("Nenhuma porta serial disponível");
-  }
+  // Substitua "COM3" pela porta serial correta do seu Arduino
+  String portName = "COM8";
+  myPort = new Serial(this, portName, 9600);
 }
 
 void draw() {
+  background(255);
 
-  if (dataReceived) {
-    println("Cor recebida: R=" + redValue + ", G=" + greenValue + ", B=" + blueValue);
+  // Define a cor do círculo baseado nos valores RGB
+  fill(redValue, greenValue, blueValue);
 
-    background(redValue, greenValue, blueValue);
-  }
-  else {
-    println("valor não recebido");
-    background(255);
-  }
+  // Desenha um círculo no centro da tela
+  ellipse(width / 2, height / 2, 200, 200);
+
+  // Exibe os valores RGB
+  fill(0);
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  String colorText = "R: " + redValue + " G: " + greenValue + " B: " + blueValue;
+  text(colorText, width / 2, height / 2 + 150);
 }
 
 void serialEvent(Serial myPort) {
-
+  // Leitura da linha recebida via Serial
   String inString = myPort.readStringUntil('\n');
   if (inString != null) {
     inString = trim(inString);
@@ -45,8 +39,58 @@ void serialEvent(Serial myPort) {
       redValue = int(rgbValues[0]);
       greenValue = int(rgbValues[1]);
       blueValue = int(rgbValues[2]);
-      
-      dataReceived = true;
     }
   }
-}  
+}
+
+//import processing.serial.*;
+
+//Serial port;
+//int redValue = 0;
+//int greenValue = 0;
+//int blueValue = 0;
+
+//boolean dataReceived = false;
+
+//void setup() {
+//  size(400, 400);
+
+//  String[] portList = Serial.list();
+
+//  if (portList.length > 0) {
+
+//    String portName = "COM8";
+
+//    port = new Serial(this, portName, 9600);
+//  } else {
+//    println("Nenhuma porta serial disponível");
+//  }
+//}
+
+//void draw() {
+
+//  if (dataReceived) {
+//    println("Cor recebida: R=" + redValue + ", G=" + greenValue + ", B=" + blueValue);
+
+//    background(redValue, greenValue, blueValue);
+//  } else {
+//    println("valor não recebido");
+//    background(255);
+//  }
+//}
+
+//void serialEvent(Serial myPort) {
+
+//  String inString = myPort.readStringUntil('\n');
+//  if (inString != null) {
+//    inString = trim(inString);
+//    String[] rgbValues = split(inString, ',');
+//    if (rgbValues.length == 3) {
+//      redValue = int(rgbValues[0]);
+//      greenValue = int(rgbValues[1]);
+//      blueValue = int(rgbValues[2]);
+
+//      dataReceived = true;
+//    }
+//  }
+//}
--- RGBW/RGBW.ino
+++ RGBW/RGBW.ino
@@ -1,44 +1,44 @@
 
-#define s0 8  
-#define s1 9
-#define s2 10
-#define s3 11
-#define out 12
+// Pinos de controle do TCS3200
+#define S0 8
+#define S1 9
+#define S2 10
+#define S3 11
+#define sensorOut 12
 
-int  Red=0, Blue=0, Green=0;  
+void setup() {
+  // Configura os pinos de controle do TCS3200
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+  pinMode(sensorOut, INPUT);
 
-void setup() 
-{
-   pinMode(s0,OUTPUT);    
-   pinMode(s1,OUTPUT);
-   pinMode(s2,OUTPUT);
-   pinMode(s3,OUTPUT);
-   pinMode(out,INPUT);
 
-     
-   
-   digitalWrite(s0, HIGH);
-  digitalWrite(s1, LOW); 
-   
-   Serial.begin(9600);
+  // Configura o sensor TCS3200 para a frequência de 20%
+  digitalWrite(S0, HIGH);
+  digitalWrite(S1, LOW);
+
+  // Inicializa a comunicação serial
+  Serial.begin(9600);
+
 }
 
-void loop(){
-
+void loop() {
   // Medir a cor vermelha
-  digitalWrite(s2, LOW);
-  digitalWrite(s3, LOW);
-  int redFrequency = pulseIn(out, LOW);
+  digitalWrite(S2, LOW);
+  digitalWrite(S3, LOW);
+  int redFrequency = pulseIn(sensorOut, LOW);
 
   // Medir a cor verde
-  digitalWrite(s2, HIGH);
-  digitalWrite(s3, HIGH);
-  int greenFrequency = pulseIn(out, LOW);
+  digitalWrite(S2, HIGH);
+  digitalWrite(S3, HIGH);
+  int greenFrequency = pulseIn(sensorOut, LOW);
 
   // Medir a cor azul
-  digitalWrite(s2, LOW);
-  digitalWrite(s3, HIGH);
-  int blueFrequency = pulseIn(out, LOW);
+  digitalWrite(S2, LOW);
+  digitalWrite(S3, HIGH);
+  int blueFrequency = pulseIn(sensorOut, LOW);
 
   // Calcular os valores RGB (ajuste conforme necessário)
   int redValue = map(redFrequency, 0, 255, 0, 255);
@@ -60,51 +60,118 @@ void loop(){
   Serial.print(",");
   Serial.println(blueValue);
 
+
+  // Atraso para evitar leitura constante
   delay(1000);
-  
-  // GetColors();                                     
-                                                   
-  // if (Red > 80 && Red < 125  &&  Green > 90 && Green < 125   &&  Blue > 80 && Blue < 125) 
-  //     Serial.println("Black");                    
-      
-  // else if (red > 12 && red < 30   &&  grn > 40 && grn < 70    &&  blu > 33 && blu < 70)    
-  //     Serial.println("Red");
-
-  // else if (Blue<Green && Blue<Red && Blue<20)  
-  //     Serial.println("Blue");
-
-  // else if (Green<Red && Green-Blue<= 8)          
-  //     Serial.println("Green");                  
-
-  // else
-  //    Serial.println("Unknown");                
-
-
-  // delay(2000);                                  
-  
-  // Serial.print(Red);
-  //   Serial.print(",");
-  //   Serial.print(Green);
-  //   Serial.print(",");
-  //   Serial.println(Blue);
-  
 }
 
 
-// void GetColors()  
-// {    
-//   digitalWrite(s2,  LOW);                                           
-//   digitalWrite(s3, LOW);                                           
-//   Red = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);       
-//   delay(20);  
+// #define s0 8  
+// #define s1 9
+// #define s2 10
+// #define s3 11
+// #define out 12
 
-//   digitalWrite(s3, HIGH);                                        
-//   Blue = pulseIn(out, digitalRead(out) == HIGH ? LOW  : HIGH);
-//   delay(20); 
+// int  Red=0, Blue=0, Green=0;  
 
-//   digitalWrite(s2, HIGH);  
-//   Green = pulseIn(out,  digitalRead(out) == HIGH ? LOW : HIGH);
-//   delay(20);  
+// void setup() 
+// {
+//    pinMode(s0,OUTPUT);    
+//    pinMode(s1,OUTPUT);
+//    pinMode(s2,OUTPUT);
+//    pinMode(s3,OUTPUT);
+//    pinMode(out,INPUT);
+
+     
+   
+//    digitalWrite(s0, HIGH);
+//   digitalWrite(s1, LOW); 
+   
+//    Serial.begin(9600);
 // }
+
+// void loop(){
+
+//   // Medir a cor vermelha
+//   digitalWrite(s2, LOW);
+//   digitalWrite(s3, LOW);
+//   int redFrequency = pulseIn(out, LOW);
+
+//   // Medir a cor verde
+//   digitalWrite(s2, HIGH);
+//   digitalWrite(s3, HIGH);
+//   int greenFrequency = pulseIn(out, LOW);
+
+//   // Medir a cor azul
+//   digitalWrite(s2, LOW);
+//   digitalWrite(s3, HIGH);
+//   int blueFrequency = pulseIn(out, LOW);
+
+//   // Calcular os valores RGB (ajuste conforme necessário)
+//   int redValue = map(redFrequency, 0, 255, 0, 255);
+//   int greenValue = map(greenFrequency, 0, 255, 0, 255);
+//   int blueValue = map(blueFrequency, 0, 255, 0, 255);
+
+//   // Mostrar os valores no Serial Monitor (opcional)
+//   Serial.print("R:");
+//   Serial.print(redValue);
+//   Serial.print(" G:");
+//   Serial.print(greenValue);
+//   Serial.print(" B:");
+//   Serial.println(blueValue);
+
+//   // Enviar os valores RGB para o Processing via Serial
+//   Serial.print(redValue);
+//   Serial.print(",");
+//   Serial.print(greenValue);
+//   Serial.print(",");
+//   Serial.println(blueValue);
+
+//   delay(1000);
+  
+//   // GetColors();                                     
+                                                   
+//   // if (Red > 80 && Red < 125  &&  Green > 90 && Green < 125   &&  Blue > 80 && Blue < 125) 
+//   //     Serial.println("Black");                    
+      
+//   // else if (red > 12 && red < 30   &&  grn > 40 && grn < 70    &&  blu > 33 && blu < 70)    
+//   //     Serial.println("Red");
+
+//   // else if (Blue<Green && Blue<Red && Blue<20)  
+//   //     Serial.println("Blue");
+
+//   // else if (Green<Red && Green-Blue<= 8)          
+//   //     Serial.println("Green");                  
+
+//   // else
+//   //    Serial.println("Unknown");                
+
+
+//   // delay(2000);                                  
+  
+//   // Serial.print(Red);
+//   //   Serial.print(",");
+//   //   Serial.print(Green);
+//   //   Serial.print(",");
+//   //   Serial.println(Blue);
+  
+// }
+
+
+// // void GetColors()  
+// // {    
+// //   digitalWrite(s2,  LOW);                                           
+// //   digitalWrite(s3, LOW);                                           
+// //   Red = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);       
+// //   delay(20);  
+
+// //   digitalWrite(s3, HIGH);                                        
+// //   Blue = pulseIn(out, digitalRead(out) == HIGH ? LOW  : HIGH);
+// //   delay(20); 
+
+// //   digitalWrite(s2, HIGH);  
+// //   Green = pulseIn(out,  digitalRead(out) == HIGH ? LOW : HIGH);
+// //   delay(20);  
+// // }
 
 
